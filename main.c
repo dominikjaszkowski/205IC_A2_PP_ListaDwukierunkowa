fed2e58421c3a85d1ee670d9d6faceb8217eec38
Dominik <ja-do@wp.pl>
2021-02-03 19:15:56 +0100

    main

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 #include "listadwukierunkowa.h"
 
 
-
 int main() {
-    int opcja=-1;
+    int opcja = -1;
     int ile;
     int srodek;
 
 
-    while(opcja!=0)
-    {
+    while (opcja != 0) {
         printf("4. Usun liczby\n");
         printf("3. dodaj do srodka.\n");
         printf("2. Wypisanie od konca i usuniecie(pierw trzeba wykonac 1).\n");
@@ -19,19 +17,18 @@ int main() {
         printf("0. Zakonczyc program.\n");
         printf("Podaj liczbe: ");
 
-        scanf("%i",&opcja);
+        scanf("%i", &opcja);
         printf("\n");
-        switch(opcja)
-        {
+        switch (opcja) {
             case 0:
                 printf("koniec programu");
                 break;
 
             case 1:
                 printf("Podaj ile chcesz miec liczb: ");
-                scanf("%i",&ile);
+                scanf("%i", &ile);
                 printf("\n");
-                for (int i=1; i<=ile; i++) {
+                for (int i = 1; i <= ile; i++) {
                     dodajnakonieclisty(i);
                     wypiszodpoczatku();
 
@@ -39,23 +36,23 @@ int main() {
                 printf("\n");
                 break;
             case 2:
-                for (int i=ile-1; i>=0; i--) {
+                for (int i = ile - 1; i >= 0; i--) {
                     wypiszodkonca();
                     usunzlisty();
                 }
                 break;
             case 3:
                 printf("Podaj liczbe do srodka: ");
-                scanf("%i",&srodek);
-                    dodajdosrodka(srodek);
-                    printf("Liczba dodana\n");
-                for (int i=ile; i>=0; i--) {
+                scanf("%i", &srodek);
+                dodajdosrodka(srodek);
+                printf("Liczba dodana\n");
+                for (int i = ile; i >= 0; i--) {
                     usunzlisty();
                 }
                 break;
 
             case 4:
-                for (int i=ile; i>=0; i--) {
+                for (int i = ile; i >= 0; i--) {
                     usunzlisty();
                 }
                 printf("Liczby usuniete\n");
@@ -68,7 +65,6 @@ int main() {
 
         }
     }
-
 
 
 }
