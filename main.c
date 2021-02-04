75b0b6df51697fa1997d52fb10663fe0870133c3
Dominik <ja-do@wp.pl>
2021-02-04 01:17:23 +0100

    main

--- main.c
+++ main.c
@@ -15,13 +15,15 @@ int main(){
         wybor=Menu();
         switch(wybor){
             case 1 :
+                printf("Wypisana lista\n");
                 pokazliste(glowa);
+                printf("\n");
                 break;
             case 2 :
-                glowa = dodajNaPoczatku(glowa, wczytajLiczbe("Podaj liczbe do dodania: \n"));
+                glowa = dodajNaPoczatku(glowa, wczytajLiczbe("Podaj liczbe do dodania na poczatek: \n"));
                 break;
             case 3 :
-                glowa = dodajNaKoniec(glowa, wczytajLiczbe("Podaj liczbe do dodania: \n"));
+                glowa = dodajNaKoniec(glowa, wczytajLiczbe("Podaj liczbe do dodania na koniec: \n"));
                 break;
             case 4 :
                 glowa = usun(glowa, wczytajLiczbe("Podaj element, ktory chcesz usunac: \n"));
